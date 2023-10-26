6b707d3c9c8b3dc3fe6d048516934de2a36dafbd
Igor Ityanagui de Andrade <igorityanagui@hotmail.com>
2023-10-26 20:14:32 -0300

    Fase do labirinto tile

--- Assets/Scripts/Puzzles/SiddingGame.cs
+++ Assets/Scripts/Puzzles/SiddingGame.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class SiddingGame : MonoBehaviour
 {
 
 
