07e16194071840c6beccb055c6752f9761aa209e
Igor Ityanagui de Andrade <igorityanagui@hotmail.com>
2023-11-04 20:40:50 -0300

    Sprites do inimigo Raiva, e tileset do deserto

--- Assets/Scripts/Botao.cs
+++ Assets/Scripts/Botao.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Botao : MonoBehaviour
 {
+    private Animator anim;
+
     public static bool pisou;
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,13 +21,17 @@ public class Botao : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Caixa")
-        pisou = true;
+        {
+            anim.SetBool("Botao", true);
+            pisou = true; 
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Caixa")
         {
+            anim.SetBool("Botao", false);
             pisou = false;
         }
     }
