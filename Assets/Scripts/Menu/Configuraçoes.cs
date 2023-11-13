afeea39078552bf701382ee007dc7f2ce07e0103
Igor Ityanagui de Andrade <igorityanagui@hotmail.com>
2023-11-12 21:11:48 -0300

    Menu ajustes

---
+++ Assets/Scripts/Menu/Configuraçoes.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+public class Configuraçoes : MonoBehaviour
+{
+
+    //Variáveis
+    public bool     isFullscreen;
+    public int      resolutionIndex;
+    public int      qualityTexture;
+    public float    musicVolume;
+
+    //Objetos
+    public Toggle fullScreenToogle;
+    public Dropdown resolutionDropdown;
+    public Dropdown qualityTextureDropdown;
+    public Slider musicVolumeSlider;
+
+    public Resolution[] resolutions;
+
+    private void OnEnable()
+    {
+        resolutions = Screen.resolutions;
+        foreach (Resolution reso in resolutions)
+        {
+            resolutionDropdown.options.Add(new Dropdown.OptionData(reso.ToString()));
+        }
+
+        // o chamado das funções
+        fullScreenToogle.onValueChanged.AddListener(delegate { OnFullScreenToggle(); });
+        resolutionDropdown.onValueChanged.AddListener(delegate { OnResolutionChange(); });
+        qualityTextureDropdown.onValueChanged.AddListener(delegate { OnTextureQualityChange(); });
+        musicVolumeSlider.onValueChanged.AddListener(delegate { OnMusicVolumeChange(); });
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnFullScreenToggle()
+    {
+
+    }
+
+    public void OnResolutionChange()
+    {
+
+    }
+
+    public void OnTextureQualityChange()
+    {
+
+    }
+
+    public void OnMusicVolumeChange()
+    {
+
+    }
+
+
+
+}
