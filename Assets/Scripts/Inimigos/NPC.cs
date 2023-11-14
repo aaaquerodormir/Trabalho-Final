7c6f5847465cf7ceea4df0e6168353b8f5f6d621
Igor Ityanagui de Andrade <igorityanagui@hotmail.com>
2023-11-14 20:24:41 -0300

    Dialogo

---
+++ Assets/Scripts/Inimigos/NPC.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    public GameObject painelDialogo;
+    public Text textoDialogo;
+    public string[] dialogo;
+    private int index;
+
+    public GameObject contButton;
+    public float wordSpeed;
+    public bool playerIsClose;
+
+    private bool teclaIPressionadaNoColisor;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I) && playerIsClose && !teclaIPressionadaNoColisor)
+        {
+            if(painelDialogo.activeInHierarchy)
+            {
+                zeroText();
+            }
+            else
+            {
+                painelDialogo.SetActive(true);
+                StartCoroutine(Digitando());
+                teclaIPressionadaNoColisor = true;
+            }
+        }
+        
+        if(textoDialogo.text == dialogo[index])
+        {
+            contButton.SetActive(true);
+        }
+
+    }
+
+    public void zeroText()
+    {
+        textoDialogo.text = "";
+        index = 0;
+        painelDialogo.SetActive(false);
+        teclaIPressionadaNoColisor = false;
+    }
+
+    IEnumerator Digitando()
+    {
+        foreach(char letter in dialogo[index].ToCharArray())
+        {
+            textoDialogo.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void ProximaLinha()
+    {
+
+        contButton.SetActive(false);
+
+        if(index < dialogo.Length - 1)
+        {
+            index++;
+            textoDialogo.text = "";
+            StartCoroutine(Digitando());
+        }
+        else
+        {
+            zeroText();
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerIsClose = true; 
+        }
+
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = false;
+            zeroText();
+        }
+    }
+}
