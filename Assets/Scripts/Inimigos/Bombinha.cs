e6dd18fdf14042bac5bbbe7c03f1460bd34bcd92
Igor Ityanagui de Andrade <igorityanagui@hotmail.com>
2023-11-24 15:00:01 -0300

    Ajustes

--- Assets/Scripts/Inimigos/Bombinha.cs
+++ Assets/Scripts/Inimigos/Bombinha.cs
@@ -4,37 +4,120 @@ using UnityEngine;
 
 public class Bombinha : MonoBehaviour
 {
-    [SerializeField]
-    private Transform alvo;
+    public float speed = 3f; // Velocidade de movimento do inimigo
+    public float visionRadius = 5f; // Raio de visão do inimigo
+    public float attackRadius = 1f; // Raio de ataque do inimigo
+    public float attackDelay = 3f; // Tempo de atraso antes de ativar o ataque
+    public float damage = 7f; // Quantidade de dano a ser causada
+
+    public Animator animator;
+    private Vector2 movementDirection = Vector2.zero;
+
+    private Transform player; // Referência ao transform do jogador
+    private bool hasAttacked = false;
 
     [SerializeField]
     private float distanciaMinima;
 
-    [SerializeField]
-    private float velocidadeMovimento;
-
-    [SerializeField]
-    private Rigidbody2D rigidbody;
-
-     void Update()
+    void Start()
     {
-        Vector2 posicaoAlvo = this.alvo.position;
-        Vector2 posicaoAtual = this.transform.position;
+        player = GameObject.FindGameObjectWithTag("Player").transform; // Encontrar o jogador pela tag
+        animator = GetComponent<Animator>();
+    }
 
-        float distancia = Vector2.Distance(posicaoAtual, posicaoAlvo);
-        if (distancia >= this.distanciaMinima)
+    void Update()
+    {
+        if (!hasAttacked)
         {
+            float distanceToPlayer = Vector2.Distance(transform.position, player.position);
 
-            Vector2 direcao = posicaoAlvo - posicaoAtual;
-            direcao = direcao.normalized;
+            // Verificar se o jogador está dentro do campo de visão
+            if (distanceToPlayer < visionRadius && distanceToPlayer > distanciaMinima)
+            {
+                if (movementDirection != Vector2.zero)
+                {
+                    animator.SetFloat("Horizontal", movementDirection.x);
+                    animator.SetFloat("Vertical", movementDirection.y);
+                }
 
-            this.rigidbody.velocity = (this.velocidadeMovimento * direcao);
-            Debug.Log("this.rigibody.velocity");
-        }
-        else
-        {
-            //Parar de mover
-            this.rigidbody.velocity = Vector2.zero; // (0,0)
+                // Verificar se o jogador está dentro do raio de ataque
+                if (distanceToPlayer < attackRadius)
+                {
+                    StartCoroutine(AttackTimer());
+                    MoveTowardsPlayer(distanceToPlayer);
+                }
+                else
+                {
+                    // Se o jogador estiver dentro do campo de visão, perseguir o jogador
+                    MoveTowardsPlayer(distanceToPlayer);
+                }
+            }
         }
     }
+
+    void MoveTowardsPlayer(float distanceToPlayer)
+    {
+        // Ativar animação de movimento
+        if (distanceToPlayer > attackRadius) // Se o jogador não estiver dentro do raio de ataque
+            animator.SetFloat("Speed", speed / distanceToPlayer);
+
+        // Se o jogador estiver dentro do campo de visão, perseguir o jogador
+        movementDirection = (player.position - transform.position).normalized;
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    }
+
+    IEnumerator AttackTimer()
+    {
+        // Ativar animação de carga do ataque
+        animator.SetTrigger("AttackCharge");
+
+        // Aguardar o tempo de atraso antes de ativar o ataque
+        yield return new WaitForSeconds(attackDelay);
+
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        // Verificar se o jogador ainda está dentro do raio de ataque antes de atacar
+        if (distanceToPlayer < attackRadius)
+        {
+            // Ativar animação de liberação do ataque
+            animator.SetTrigger("AttackRelease");
+
+            // Ativar o colisor ou a lógica de dano aqui
+            DealDamage();
+        }
+
+        // Marcador de que o inimigo já atacou, para evitar ataques repetidos
+        hasAttacked = true;
+
+        // Destruir o inimigo após o ataque
+        Destroy(gameObject);
+    }
+
+    void DealDamage()
+    {
+        MiroHp miroHp = player.GetComponent<MiroHp>();
+        miroHp.TakeDamage(7);
+    }
+
+    void OnDrawGizmos()
+    {
+        // Visualizar o raio de visão
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, visionRadius);
+
+        // Visualizar o raio de ataque
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, distanciaMinima);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Visualizar o raio de ataque quando o objeto estiver selecionado
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+    }
+
 }
